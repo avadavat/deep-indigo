793e5350c542757d6a1484c4399cea9775f07898
Joe Badlato <joebadlato@gmail.com>
2020-03-16 21:56:10 -0400

    Update npm start script

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@
  * leftovers-again. No edits necessary.
  */
 import * as LeftoversAgain from 'leftovers-again';
-import * as DeepIndigo from './bot';
+import { DeepIndigo } from './bot';
 import * as metadata from '../package.json';
 export class X {}
-LeftoversAgain.start(metadata, new DeepIndigo.DeepIndigo());
+LeftoversAgain.start(metadata, new DeepIndigo());
